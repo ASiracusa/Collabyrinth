705cf27329a7963b743e7e5dc9d8e73508d0ccb6
Maximo <mkbcurb@gmail.com>
2020-02-13 21:35:23 -0500

    AI scraped

--- Collabyrinth/Assets/Resources/Scripts/AI (A star).cs
+++ Collabyrinth/Assets/Resources/Scripts/AI (A star).cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using PriorityQueue;
@@ -161,3 +161,4 @@ public class AI {
     }
     
 }
+*/
