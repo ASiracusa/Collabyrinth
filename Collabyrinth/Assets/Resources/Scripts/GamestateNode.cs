dcdc1aa84127ad3dfdb2a5ea1f6a5dbceb30a532
vigne <vignet009@gmail.com>
2020-02-13 19:27:32 -0500

    Fix GamestateNode

--- Collabyrinth/Assets/Resources/Scripts/GamestateNode.cs
+++ Collabyrinth/Assets/Resources/Scripts/GamestateNode.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class GamestateNode : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Tile[][] board;
+    int length;
+
+    public GamestateNode(int length)
     {
-        
+        this.length = length;
+        board = new Tile[length][length];
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < length; j++)
+            {
+                board[i][j] = new Tile(i, j, true);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
