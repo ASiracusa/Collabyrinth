2285f9a4b25508aba726544644e4d0ea98d9e6ba
Alex <adruzenko03@gmail.com>
2020-02-13 19:42:53 -0500

    Finished tile

--- Collabyrinth/Assets/Resources/Scripts/Tile.cs
+++ Collabyrinth/Assets/Resources/Scripts/Tile.cs
@@ -2,24 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile : MonoBehaviour
+public class Tile
 {
-    //Player player;
-    bool exists;
-    //Player[] bridge
-    int[] pos;
+    public Player player;
+    public bool exists;
+    public Player[] bridge
+    public int[] pos;
+    public final int UP= 0;
+    public final int RIGHT= 1;
+    public final int DOWN= 2;
+    public final int LEFT= 3;
 
-    public Tile(int row, int col, bool ex)
+    public Tile(int row, int col, bool ex, Player pp)
     {
+        player=pp;
         pos={x,y};
         exists=ex;
+        bridge= new Player[4];
     }    
-    public void TakeBridge()
-    {}
-    public void PutBridge()
-    {}
+    public bool TakeBridge(int pos)
+    {
+        if(bridge[pos]==null)
+            return false;
+        bridge[pos]=null;
+        return true;
+    }
+    public bool PutBridge(int pos, Player briPl)
+    {
+        if(bridge[pos]==null){
+            bridge[pos]=briPl;
+            return true;
+        }
+        return false;
+
+    }
     public void RemovePlayer()
-    {}
-    public void PutPlayer(){}
+    {
+        player=null;
+    }
+    public bool PutPlayer(Player newPl)
+    {
+        if(player==null){
+            player=newPl
+            return true;
+        }
+        return false;
+    }
     
 }
